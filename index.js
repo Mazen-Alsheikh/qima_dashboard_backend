245078be2a1be38d63ffc8859f0c0a7a04622aa7
Mazen Alsheikh <mazenkaya445566@gmail.com>
2025-06-27 18:23:03 +0300

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const db = require("./connect");
+const { getProjects, addProject, deleteProject, editProject } = require("./services/projectServices");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+try {
+    db.connect();
+    console.log("Database Connected");
+} catch (error) {
+    console.error(error.message);
+}
+
+app.get("/api/projects", getProjects);
+app.post("/api/project/add", addProject);
+app.post("/api/project/delete", deleteProject);
+app.post("/api/project/edit", editProject);
+
+const port = process.env.SERVER_PORT;
+
+app.listen(port, () => {
+    console.log("Server is online");
+});
---
+++ services/projectServices.js
@@ -0,0 +1,71 @@
+const db = require("../connect");
+
+exports.getProjects = (req, res) => {
+
+    db.query("SELECT * FROM projects", (error, result) => {
+
+        if (error) return res.status(500).json({message: "خطأ في قاعدة البيانات"});
+
+        if (result.length === 0) return res.status(404).json({message: "لايوجد معاملات"});
+
+        return res.status(200).json({message: "تم جلب المعاملات بنجاح", data: result});
+    })
+    
+}
+
+exports.addProject = (req, res) => {
+    
+    const {name, type, hood, longitude, latitude} = req.body;
+
+    if (!name || !type || !hood || !longitude || !latitude) {
+        return res.json({message: "يجب إكمال جميع الحقول"});
+    }
+
+    db.query("SELECT * FROM projects WHERE name = ?", name, (error, result) => {
+        
+        if (error) return res.status(500).json({message: "خطأ في قاعدة البيانات"});
+
+        if (result.length > 0) return res.status(404).json({message: "المعاملة موجودة بالفعل"});
+
+        db.query("INSERT INTO projects(name, hood, sort_type, longitude, latitude) VALUES (?, ?, ?, ?, ?)", [name, hood, type, longitude, latitude], (error, result) => {
+            
+            if (error) return res.status(500).json({message: "خطأ في قاعدة البيانات", error: error.message});
+
+            res.status(201).json({message: "تم إضافة معاملة جديدة بنجاح"});
+
+        });
+    })
+
+}
+
+exports.deleteProject = (req, res) => {
+    const name = req.body.name;
+    db.query("DELETE FROM projects WHERE name = ?", name, (error, result) => {
+        if (error) return res.status(500).json({message: "خطأ في قاعدة البيانات"});
+        res.status(200).json({message: "تم حذف المعاملة بنجاح"});
+    })
+}
+
+exports.editProject = (req, res) => {
+    const {name, type, hood, longitude, latitude} = req.body;
+
+    if (!name || !type || !hood || !longitude || !latitude) {
+        return res.json({message: "يجب إكمال جميع الحقول"});
+    }
+
+    db.query("SELECT * FROM projects WHERE name = ?", name, (error, result) => {
+        
+        if (error) return res.status(500).json({message: "خطأ في قاعدة البيانات"});
+        
+        if (result.length === 0) return res.status(404).json({message: "المعاملة غير موجودة"});
+
+        db.query("UPDATE projects SET name = ?, hood = ?, sort_type = ?, longitude = ?, latitude = ? WHERE name = ?", [name, hood, type, longitude, latitude, name], (error, result) => {
+    
+            if (error) return res.status(500).json({message: "خطأ في قاعدة البيانات", error: error.message});
+
+            res.status(200).json({message: "تم تعديل المعاملة بنجاح"});
+
+        });
+    })
+
+}
